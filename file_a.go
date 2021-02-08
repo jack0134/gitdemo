57a9e894179c0c06bc360efad6be731441e8d540
Jack <>
2021-02-08 14:17:35 +0800

    M: Clear Content

--- file_a.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func printHelloA() {
-	fmt.Println("Hello A!")
-
-	fmt.Println("Update A-1!")
-}
--- main.go
+++ main.go
@@ -4,12 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello!")
-
-	fmt.Println("Some Improvement")
-
-	fmt.Println("Update main 1!")
-
-	printHelloA()
-
-	fmt.Println("Update main 2!")
 }
